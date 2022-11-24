e003ce1c6d922418374e248a063ec86fbf76cfaa
VianaTameira <190030690@stikom-bali.ac.id>
2022-11-25 00:24:39 +0700

    add AirplaneController class

---
+++ src/main/java/com/binar/finalproject/BEFlightTicket/controller/AirplaneController.java
@@ -0,0 +1,9 @@
+package com.binar.finalproject.BEFlightTicket.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/airplane")
+public class AirplaneController {
+}
