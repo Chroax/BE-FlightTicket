dfbe283415cb08059c780218beb2636c5854d87b
Cahyadi Surya Nugraha <cahyadisn6@gmail.com>
2022-11-24 20:29:56 +0700

    add RoleController class

---
+++ src/main/java/com/binar/finalproject/BEFlightTicket/controller/RoleController.java
@@ -0,0 +1,16 @@
+package com.binar.finalproject.BEFlightTicket.controller;
+
+import com.binar.finalproject.BEFlightTicket.dto.MessageModel;
+import com.binar.finalproject.BEFlightTicket.dto.RoleRequest;
+import com.binar.finalproject.BEFlightTicket.dto.RoleResponse;
+import com.binar.finalproject.BEFlightTicket.service.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/role")
+public class RoleController {
+
+}
