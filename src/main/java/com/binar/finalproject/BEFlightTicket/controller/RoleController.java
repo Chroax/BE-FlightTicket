1b4508caa2152aa3bf47006582fced079a7ac411
Cahyadi Surya Nugraha <cahyadisn6@gmail.com>
2022-11-24 21:16:48 +0700

    Update RoleController.java
    - add MediaType.JSON so response that will be produced will be converted into JSON format.

--- src/main/java/com/binar/finalproject/BEFlightTicket/controller/RoleController.java
+++ src/main/java/com/binar/finalproject/BEFlightTicket/controller/RoleController.java
@@ -6,6 +6,7 @@ import com.binar.finalproject.BEFlightTicket.dto.RoleResponse;
 import com.binar.finalproject.BEFlightTicket.service.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,7 +19,7 @@ public class RoleController {
     @Autowired
     private RoleService roleService;
 
-    @PostMapping("/add")
+    @PostMapping(value = "/add", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<MessageModel> createRole(@RequestBody RoleRequest roleRequest)
     {
@@ -40,7 +41,7 @@ public class RoleController {
         }
     }
 
-    @GetMapping("/get-all")
+    @GetMapping(value = "/get-all", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<MessageModel> getAllRole()
     {
         MessageModel messageModel = new MessageModel();
@@ -58,7 +59,7 @@ public class RoleController {
         }
     }
 
-    @PutMapping("/update/{roleName}")
+    @PutMapping(value = "/update/{roleName}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<MessageModel> updateRole(@PathVariable String roleName, @RequestBody RoleRequest roleRequest)
     {
         MessageModel messageModel = new MessageModel();
@@ -79,7 +80,7 @@ public class RoleController {
         }
     }
 
-    @DeleteMapping("/delete/{roleName}")
+    @DeleteMapping(value = "/delete/{roleName}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<MessageModel> deleteRole(@PathVariable String roleName)
     {
         MessageModel messageModel = new MessageModel();
