da5b83e2eda07658ba139d19b236dbcf3f04fc11
VianaTameira <190030690@stikom-bali.ac.id>
2022-11-25 00:21:09 +0700

    update AirplanesRepository
    - tambah query
    - harusnya commit bareng service tapi ketinggalan

--- src/main/java/com/binar/finalproject/BEFlightTicket/repository/AirplanesRepository.java
+++ src/main/java/com/binar/finalproject/BEFlightTicket/repository/AirplanesRepository.java
@@ -7,5 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface AirplanesRepository extends JpaRepository <Airplanes, String> {
-
+    @Query("SELECT r FROM Airplanes r WHERE LOWER(r.airplaneName) LIKE LOWER(:airplaneName)")
+    Airplanes findByName(@Param("airplaneName") String roleName);
 }
