220bb43f3ff7744a3123978cadd7737cae13202a
Cahyadi Surya Nugraha <cahyadisn6@gmail.com>
2022-11-24 20:34:56 +0700

    add endpoint /role/add
    - Create Feature Add Role

--- src/main/java/com/binar/finalproject/BEFlightTicket/controller/RoleController.java
+++ src/main/java/com/binar/finalproject/BEFlightTicket/controller/RoleController.java
@@ -9,8 +9,35 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/role")
 public class RoleController {
+
+    @Autowired
+    private RoleService roleService;
+
+    @PostMapping("/add")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<MessageModel> createRole(@RequestBody RoleRequest roleRequest)
+    {
+        MessageModel messageModel = new MessageModel();
+        RoleResponse roleResponse = roleService.registerRole(roleRequest);
+        if(roleResponse == null)
+        {
+            messageModel.setStatus(HttpStatus.BAD_REQUEST.value());
+            messageModel.setMessage("Failed to create roles");
+            return ResponseEntity.badRequest().body(messageModel);
+        }
+        else
+        {
+            messageModel.setStatus(HttpStatus.CREATED.value());
+            messageModel.setMessage("Create new roles");
+            messageModel.setData(roleResponse);
+            return ResponseEntity.ok().body(messageModel);
+
+        }
+    }
 
 }
