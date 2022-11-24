5d0030ff7fd40d21aae83136ce9c299941b550c3
Cahyadi Surya Nugraha <cahyadisn6@gmail.com>
2022-11-24 23:51:46 +0700

    add UserController class

--- src/main/java/com/binar/finalproject/BEFlightTicket/service/impl/UserServiceImpl.java
+++ src/main/java/com/binar/finalproject/BEFlightTicket/service/impl/UserServiceImpl.java
@@ -10,11 +10,13 @@ import com.binar.finalproject.BEFlightTicket.repository.RoleRepository;
 import com.binar.finalproject.BEFlightTicket.repository.UserRepository;
 import com.binar.finalproject.BEFlightTicket.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
