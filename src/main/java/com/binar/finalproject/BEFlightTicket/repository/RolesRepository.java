0e709edc30c5e78483ae576050dd4f6624ba6668
Cahyadi Surya Nugraha <cahyadisn6@gmail.com>
2022-11-24 19:23:36 +0700

    add roles repository

---
+++ src/main/java/com/binar/finalproject/BEFlightTicket/repository/RolesRepository.java
@@ -0,0 +1,8 @@
+package com.binar.finalproject.BEFlightTicket.repository;
+
+import com.binar.finalproject.BEFlightTicket.model.Roles;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolesRepository extends JpaRepository<Roles, Integer> {
+
+}
