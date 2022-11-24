e5c957ccd2941283cd82f1d5b39e8bc3a8815bb4
Cahyadi Surya Nugraha <cahyadisn6@gmail.com>
2022-11-24 17:22:34 +0700

    add terminals entity
    - add terminals entity
    - update airports entity
    - add one to many relation airports-terminals

--- src/main/java/com/binar/finalproject/BEFlightTicket/model/Terminals.java
+++ src/main/java/com/binar/finalproject/BEFlightTicket/model/Terminals.java
@@ -9,21 +9,21 @@ import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
-import java.util.Set;
 
 @Entity
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "airports")
-public class Airports {
+@Table(name = "terminals")
+public class Terminals {
     @Id
-    @Column(name = "iata_code", columnDefinition = "CHAR(3)")
-    private String iataCode;
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "terminal_id")
+    private Integer terminalId;
 
-    @Column(name = "airport_name", nullable = false)
-    private String airportName;
+    @Column(name = "terminal_name", columnDefinition = "CHAR(3)", nullable = false)
+    private String terminalName;
 
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @Column(name = "created_at", nullable = false, updatable = false, insertable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
@@ -35,9 +35,6 @@ public class Airports {
     private LocalDateTime modifiedAt;
 
     @ManyToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name="city_code", nullable = false)
-    private Cities citiesAirport;
-
-    @OneToMany(mappedBy = "airportsTerminals", cascade = CascadeType.ALL)
-    private Set<Terminals> terminals;
+    @JoinColumn(name="iata_code", nullable = false)
+    private Airports airportsTerminals;
 }
