2607d0f3d15fe488329b2beb5d2a85bc5eee63d0
Cahyadi Surya Nugraha <cahyadisn6@gmail.com>
2022-11-25 16:47:40 +0700

    add passport repository
    - add passport repository
    - add custom query findAllPassportByTravelerList

---
+++ src/main/java/com/binar/finalproject/BEFlightTicket/repository/PassportRepository.java
@@ -0,0 +1,15 @@
+package com.binar.finalproject.BEFlightTicket.repository;
+
+import com.binar.finalproject.BEFlightTicket.model.Passport;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface PassportRepository extends JpaRepository<Passport, String> {
+
+    @Query(nativeQuery = true, value = "SELECT * FROM passport p where p.traveler_id = :travelerId")
+    List<Passport> findAllPassportByTravelerList(@Param("travelerId") UUID travelerId);
+}
