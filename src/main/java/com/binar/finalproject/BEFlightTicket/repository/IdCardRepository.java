eca7eabbd46b6bfc9a9b8b77939249278de796dc
Cahyadi Surya Nugraha <cahyadisn6@gmail.com>
2022-11-25 17:20:54 +0700

    add id-card repository
    - add id-card repository
    - add custom query findAllIdCardByTravelerList

---
+++ src/main/java/com/binar/finalproject/BEFlightTicket/repository/IdCardRepository.java
@@ -0,0 +1,15 @@
+package com.binar.finalproject.BEFlightTicket.repository;
+
+import com.binar.finalproject.BEFlightTicket.model.IDCard;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface IdCardRepository extends JpaRepository<IDCard, String> {
+
+    @Query(nativeQuery = true, value = "SELECT * FROM id_card i where i.traveler_id = :travelerId")
+    List<IDCard> findAllIdCardByTravelerList(@Param("travelerId") UUID travelerId);
+}
