3994976fcb9ccc6fc858d6e9f1b6551526ffd935
Cahyadi Surya Nugraha <cahyadisn6@gmail.com>
2022-11-25 17:23:11 +0700

    add IDCardController class

---
+++ src/main/java/com/binar/finalproject/BEFlightTicket/controller/IDCardController.java
@@ -0,0 +1,20 @@
+package com.binar.finalproject.BEFlightTicket.controller;
+
+import com.binar.finalproject.BEFlightTicket.dto.*;
+import com.binar.finalproject.BEFlightTicket.service.IdCardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/id-card")
+public class IDCardController {
+
+    @Autowired
+    IdCardService idCardService;
+
+}
