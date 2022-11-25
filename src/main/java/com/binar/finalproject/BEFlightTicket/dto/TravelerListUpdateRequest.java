db68d485c70ee5103d867db8fa8735e7a8fae689
Cahyadi Surya Nugraha <cahyadisn6@gmail.com>
2022-11-25 09:46:08 +0700

    add TravelerList dto
    - add TravelerListRequestclass
    - add TravelerListResponse class
    - add TravelerListUpdateRequest class

---
+++ src/main/java/com/binar/finalproject/BEFlightTicket/dto/TravelerListUpdateRequest.java
@@ -0,0 +1,14 @@
+package com.binar.finalproject.BEFlightTicket.dto;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class TravelerListUpdateRequest {
+    private String type;
+    private String title;
+    private String firstName;
+    private String lastName;
+    private LocalDate birthDate;
+}
