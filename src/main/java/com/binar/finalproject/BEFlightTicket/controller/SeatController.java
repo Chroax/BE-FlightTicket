2f01c00bacc7e7227caeeaf3ddbc31c810cad0b8
VianaTameira <190030690@stikom-bali.ac.id>
2022-11-25 15:24:33 +0700

    Add endpoint /seat/add
    - Create Feature Add Seat

--- src/main/java/com/binar/finalproject/BEFlightTicket/controller/SeatController.java
+++ src/main/java/com/binar/finalproject/BEFlightTicket/controller/SeatController.java
@@ -1,10 +1,43 @@
 package com.binar.finalproject.BEFlightTicket.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.binar.finalproject.BEFlightTicket.dto.MessageModel;
+import com.binar.finalproject.BEFlightTicket.dto.SeatRequest;
+import com.binar.finalproject.BEFlightTicket.dto.SeatResponse;
+import com.binar.finalproject.BEFlightTicket.service.SeatService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/seat")
 public class SeatController {
-    
+
+    @Autowired
+    private SeatService seatService;
+
+    @PostMapping(value = "/add", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<MessageModel> addSeat(@RequestBody SeatRequest seatRequest)
+    {
+        MessageModel messageModel = new MessageModel();
+        SeatResponse seatResponse = seatService.addSeat(seatRequest);
+        if (seatResponse == null)
+        {
+            messageModel.setStatus(HttpStatus.BAD_REQUEST.value());
+            messageModel.setMessage("Failed to add seat");
+            return ResponseEntity.badRequest().body(messageModel);
+        }
+        else
+        {
+            messageModel.setStatus(HttpStatus.CREATED.value());
+            messageModel.setMessage("add new seat");
+            messageModel.setData(seatResponse);
+            return ResponseEntity.ok().body(messageModel);
+        }
+    }
+
+
+
 }
