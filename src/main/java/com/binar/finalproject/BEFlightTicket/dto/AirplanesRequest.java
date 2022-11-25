79e0fe86155f249ec81fbe5f647e4cf60ec36fae
Cahyadi Surya Nugraha <cahyadisn6@gmail.com>
2022-11-25 08:58:40 +0700

    add attribute in AirplaneRequest
    - add airplaneType attribute

--- src/main/java/com/binar/finalproject/BEFlightTicket/dto/AirplanesRequest.java
+++ src/main/java/com/binar/finalproject/BEFlightTicket/dto/AirplanesRequest.java
@@ -7,11 +7,15 @@ import javax.validation.constraints.NotEmpty;
 
 @Data
 public class AirplanesRequest {
+
+    @NotEmpty(message = "Airplane type is required.")
+    private String airplaneType;
     @NotEmpty(message = "Airplane name is required.")
     private String airplaneName;
 
     public Airplanes toAirplanes(){
         return Airplanes.builder()
+                .airplaneType(this.airplaneType)
                 .airplaneName(this.airplaneName)
                 .build();
     }
