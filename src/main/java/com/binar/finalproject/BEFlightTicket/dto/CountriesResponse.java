20ebd64d8688dc22d331e7ea43ae7b0aeceb6871
AnggaAnugerahSaputro <91450155+AnggaAnugerahSaputro@users.noreply.github.com>
2022-11-25 14:20:54 +0700

    Merge branch 'angga_crud-countries' into cahyadi_crud-travelerlist

---
+++ src/main/java/com/binar/finalproject/BEFlightTicket/dto/CountriesResponse.java
@@ -0,0 +1,23 @@
+package com.binar.finalproject.BEFlightTicket.dto;
+
+import com.binar.finalproject.BEFlightTicket.model.Countries;
+import com.binar.finalproject.BEFlightTicket.model.Roles;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class CountriesResponse {
+
+    private String countryCode;
+    private String countryName;
+    private String telephoneCode;
+
+    public static CountriesResponse build(Countries countries) {
+        return CountriesResponse.builder()
+                .countryCode(countries.getCountryCode())
+                .countryName(countries.getCountryName())
+                .telephoneCode(countries.getTelephoneCode())
+                .build();
+    }
+}
