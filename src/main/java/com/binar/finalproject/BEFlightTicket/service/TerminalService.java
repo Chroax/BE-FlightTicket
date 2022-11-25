21fdad5f4fc5fb22651ac28b4ab047998aff5509
AnggaAnugerahSaputro <91450155+AnggaAnugerahSaputro@users.noreply.github.com>
2022-11-25 22:08:33 +0700

    add TerminalService
    - create TerminalService in package service

---
+++ src/main/java/com/binar/finalproject/BEFlightTicket/service/TerminalService.java
@@ -0,0 +1,17 @@
+package com.binar.finalproject.BEFlightTicket.service;
+
+import com.binar.finalproject.BEFlightTicket.dto.CitiesRequest;
+import com.binar.finalproject.BEFlightTicket.dto.CitiesResponse;
+import com.binar.finalproject.BEFlightTicket.dto.TerminalRequest;
+import com.binar.finalproject.BEFlightTicket.dto.TerminalResponse;
+
+import java.util.List;
+
+public interface TerminalService {
+
+    TerminalResponse addTerminal(TerminalRequest terminalRequest);
+    List<TerminalResponse> searchAllTerminal();
+    TerminalResponse updateTerminal(TerminalRequest terminalRequest, String terminalName);
+    Boolean deleteTerminal(String terminalName);
+    TerminalResponse searchTerminalByName(String terminalName);
+}
