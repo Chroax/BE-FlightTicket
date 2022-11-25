72519eb64c2917eedd0d8263d5333935055f1f98
Cahyadi Surya Nugraha <cahyadisn6@gmail.com>
2022-11-25 14:50:43 +0700

    add endpoint /traveler-list/add
    - Create Feature Add TravelerList

--- src/main/java/com/binar/finalproject/BEFlightTicket/controller/TravelerListController.java
+++ src/main/java/com/binar/finalproject/BEFlightTicket/controller/TravelerListController.java
@@ -1,9 +1,11 @@
 package com.binar.finalproject.BEFlightTicket.controller;
 
+import com.binar.finalproject.BEFlightTicket.dto.*;
 import com.binar.finalproject.BEFlightTicket.service.TravelerListService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/traveler-list")
@@ -11,4 +13,25 @@ public class TravelerListController {
 
     @Autowired
     private TravelerListService travelerListService;
+
+    @PostMapping("/add")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<MessageModel> registerUser(@RequestBody TravelerListRequest travelerListRequest) {
+        MessageModel messageModel = new MessageModel();
+
+        TravelerListResponse travelerListResponse = travelerListService.registerTravelerList(travelerListRequest);
+        if(travelerListResponse == null)
+        {
+            messageModel.setMessage("Failed register new traveler list");
+            messageModel.setStatus(HttpStatus.BAD_REQUEST.value());
+            return ResponseEntity.badRequest().body(messageModel);
+        }
+        else
+        {
+            messageModel.setStatus(HttpStatus.OK.value());
+            messageModel.setMessage("Register new traveler list");
+            messageModel.setData(travelerListResponse);
+            return ResponseEntity.ok().body(messageModel);
+        }
+    }
 }
