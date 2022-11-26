05d659cd99b4d5235f0cb37602a302796ac3f609
VianaTameira <190030690@stikom-bali.ac.id>
2022-11-26 16:40:34 +0700

    add endpoint /payment/add
    - Create Feature Add Payment Method

--- src/main/java/com/binar/finalproject/BEFlightTicket/controller/PaymentMethodController.java
+++ src/main/java/com/binar/finalproject/BEFlightTicket/controller/PaymentMethodController.java
@@ -1,9 +1,36 @@
 package com.binar.finalproject.BEFlightTicket.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.binar.finalproject.BEFlightTicket.dto.*;
+import com.binar.finalproject.BEFlightTicket.service.PaymentMethodService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("payment")
+@RequestMapping("/payment")
 public class PaymentMethodController {
+
+    @Autowired
+    PaymentMethodService paymentMethodService;
+    @PostMapping("/add")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<MessageModel> addPayment(@RequestBody PaymentMethodRequest paymentMethodRequest) {
+        MessageModel messageModel = new MessageModel();
+
+        PaymentMethodResponse paymentMethodResponse = paymentMethodService.addPaymentMethod(paymentMethodRequest);
+        if(paymentMethodResponse == null)
+        {
+            messageModel.setMessage("Failed add new payment method");
+            messageModel.setStatus(HttpStatus.BAD_REQUEST.value());
+            return ResponseEntity.badRequest().body(messageModel);
+        }
+        else
+        {
+            messageModel.setStatus(HttpStatus.OK.value());
+            messageModel.setMessage("Add new payment method");
+            messageModel.setData(paymentMethodResponse);
+            return ResponseEntity.ok().body(messageModel);
+        }
+    }
 }
