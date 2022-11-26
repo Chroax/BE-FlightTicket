bfffe6934712b4291803dfdc80e68cfc39ded8bf
AnggaAnugerahSaputro <91450155+AnggaAnugerahSaputro@users.noreply.github.com>
2022-11-26 15:06:57 +0700

    add service add & update gates
    create service add & update gates in class GatesServiceImpl

--- src/main/java/com/binar/finalproject/BEFlightTicket/service/impl/GatesServiceImpl.java
+++ src/main/java/com/binar/finalproject/BEFlightTicket/service/impl/GatesServiceImpl.java
@@ -3,6 +3,7 @@ package com.binar.finalproject.BEFlightTicket.service.impl;
 import com.binar.finalproject.BEFlightTicket.dto.*;
 import com.binar.finalproject.BEFlightTicket.model.*;
 import com.binar.finalproject.BEFlightTicket.repository.GatesRepository;
+import com.binar.finalproject.BEFlightTicket.repository.TerminalsRepository;
 import com.binar.finalproject.BEFlightTicket.service.GatesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,15 +18,18 @@ public class GatesServiceImpl implements GatesService {
     @Autowired
     private GatesRepository gatesRepository;
 
+    @Autowired
+    private TerminalsRepository terminalsRepository;
+
     @Override
     public GatesResponse addGates(GatesRequest gatesRequest) {
         try {
-            Optional<Gates> terminals = gatesRepository.findById(gatesRequest.getTerminalId());
+            Optional<Terminals> terminals = terminalsRepository.findById(gatesRequest.getTerminalId());
             if(terminals.isPresent())
             {
                 Gates gates = Gates.builder()
                         .gateName(gatesRequest.getGateName())
-                        .terminalsGates(terminals.get().getTerminalsGates())
+                        .terminalsGates(terminals.get())
                         .build();
                 gatesRepository.saveAndFlush(gates);
                 return GatesResponse.build(gates);
@@ -51,7 +55,29 @@ public class GatesServiceImpl implements GatesService {
 
     @Override
     public GatesResponse updateGates(GatesRequest gatesRequest, String gateName) {
-        return null;
+        Gates gates = gatesRepository.findByGatesName(gateName);
+        String message = null;
+        if (gates != null) {
+            if (gatesRequest.getGateName() != null)
+                gates.setGateName(gatesRequest.getGateName());
+            if (gatesRequest.getTerminalId() != null)
+            {
+                Optional<Terminals> terminals = terminalsRepository.findById(gatesRequest.getTerminalId());
+                if(terminals.isPresent())
+                    gates.setTerminalsGates(terminals.get());
+                else
+                    message = "Terminals with this id doesnt exist";
+            }
+            if(message == null)
+                return null;
+            else
+            {
+                gatesRepository.saveAndFlush(gates);
+                return GatesResponse.build(gates);
+            }
+        } else
+            return null;
+
     }
 
     @Override
