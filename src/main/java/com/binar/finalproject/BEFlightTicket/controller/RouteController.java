76ef0f6e283545a541c30b582141b92560215782
VianaTameira <190030690@stikom-bali.ac.id>
2022-11-27 01:05:03 +0700

    create endpoint /route/get-all/{departureCity}/{arrivalCity}
    - create feature to find route by departure city and arrival city

--- src/main/java/com/binar/finalproject/BEFlightTicket/controller/RouteController.java
+++ src/main/java/com/binar/finalproject/BEFlightTicket/controller/RouteController.java
@@ -10,6 +10,8 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/route")
 public class RouteController {
@@ -34,6 +36,22 @@ public class RouteController {
             messageModel.setMessage("Add new route");
             messageModel.setData(routeResponse);
             return ResponseEntity.ok().body(messageModel);
+        }
+    }
+    @GetMapping("/get-all/{departureCity}/{arrivalCity}")
+    public ResponseEntity<MessageModel> getRouteByDepartureAndArrivalCity(@PathVariable String departureCity,@PathVariable String arrivalCity){
+        MessageModel messageModel = new MessageModel();
+        try {
+            List<RouteResponse> routeResponse = routeService.findByDepartureCityAndArrivalCity(departureCity, arrivalCity);
+            messageModel.setMessage("Success get route");
+            messageModel.setStatus(HttpStatus.OK.value());
+            messageModel.setData(routeResponse);
+            return ResponseEntity.ok().body(messageModel);
+        }catch (Exception exception)
+        {
+            messageModel.setMessage("Failed get route");
+            messageModel.setStatus(HttpStatus.BAD_GATEWAY.value());
+            return ResponseEntity.status(HttpStatus.BAD_GATEWAY.value()).body(messageModel);
         }
     }
 }
