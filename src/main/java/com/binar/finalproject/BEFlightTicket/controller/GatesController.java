fddf648554e8c89a1cb60ff62b0fef88f96b89b5
AnggaAnugerahSaputro <91450155+AnggaAnugerahSaputro@users.noreply.github.com>
2022-11-26 16:36:25 +0700

    add endpoint /gates/get-all
    -create endpoint /gates/get-all in class GatesController

--- src/main/java/com/binar/finalproject/BEFlightTicket/controller/GatesController.java
+++ src/main/java/com/binar/finalproject/BEFlightTicket/controller/GatesController.java
@@ -4,8 +4,11 @@ import com.binar.finalproject.BEFlightTicket.dto.*;
 import com.binar.finalproject.BEFlightTicket.service.GatesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/gates")
@@ -20,14 +23,12 @@ public class GatesController {
         MessageModel messageModel = new MessageModel();
 
         GatesResponse gatesResponse = gatesService.addGates(gatesRequest);
-        if(gatesResponse == null)
-        {
+        if(gatesResponse == null) {
             messageModel.setMessage("Failed to create Gates");
             messageModel.setStatus(HttpStatus.BAD_REQUEST.value());
             return ResponseEntity.badRequest().body(messageModel);
         }
-        else
-        {
+        else {
             messageModel.setStatus(HttpStatus.OK.value());
             messageModel.setMessage("Create new Gates");
             messageModel.setData(gatesResponse);
@@ -35,5 +36,20 @@ public class GatesController {
         }
     }
 
-
+    @GetMapping(value = "/get-all", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<MessageModel> getAllGates() {
+        MessageModel messageModel = new MessageModel();
+        try {
+            List<GatesResponse> getAllGates = gatesService.searchAllGates();
+            messageModel.setMessage("Success get all Gates");
+            messageModel.setStatus(HttpStatus.OK.value());
+            messageModel.setData(getAllGates);
+            return ResponseEntity.ok().body(messageModel);
+        }catch (Exception exception) {
+            messageModel.setMessage("Failed get all Gates");
+            messageModel.setStatus(HttpStatus.BAD_GATEWAY.value());
+            return ResponseEntity.status(HttpStatus.BAD_GATEWAY.value()).body(messageModel);
+        }
+    }
 }
