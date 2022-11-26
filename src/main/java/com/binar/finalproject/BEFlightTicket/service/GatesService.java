a30c4d852c61a6f8ccc12bb653283265f6345a9a
AnggaAnugerahSaputro <91450155+AnggaAnugerahSaputro@users.noreply.github.com>
2022-11-26 14:14:23 +0700

    add GatesService
    create class and  service in GatesService

---
+++ src/main/java/com/binar/finalproject/BEFlightTicket/service/GatesService.java
@@ -0,0 +1,14 @@
+package com.binar.finalproject.BEFlightTicket.service;
+
+import com.binar.finalproject.BEFlightTicket.dto.*;
+
+import java.util.List;
+
+public interface GatesService {
+
+    GatesResponse addGates(GatesRequest  gatesRequest);
+    List<GatesResponse> searchAllGates();
+    GatesResponse updateGates(GatesRequest gatesRequest, String gateName);
+    Boolean deleteGates(String gateName);
+    GatesResponse searchGatesByName(String gateName);
+}
