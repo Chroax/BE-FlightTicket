cfc4aaa7de9b9acf1c7afebccb925ec3b19d0688
VianaTameira <190030690@stikom-bali.ac.id>
2022-11-26 14:18:47 +0700

    fixing seat repository

--- src/main/java/com/binar/finalproject/BEFlightTicket/repository/SeatRepository.java
+++ src/main/java/com/binar/finalproject/BEFlightTicket/repository/SeatRepository.java
@@ -1,6 +1,4 @@
 package com.binar.finalproject.BEFlightTicket.repository;
-
-import com.binar.finalproject.BEFlightTicket.model.Roles;
 import com.binar.finalproject.BEFlightTicket.model.Seats;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
