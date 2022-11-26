d06f7c7e5f9a14a1ed84d6913059f64a84b88b28
AnggaAnugerahSaputro <91450155+AnggaAnugerahSaputro@users.noreply.github.com>
2022-11-26 14:30:27 +0700

    add GatesServiceImpl
    - create class GatesServiceImpl
    - create service addGates in class GatesServiceImpl

---
+++ src/main/java/com/binar/finalproject/BEFlightTicket/service/impl/GatesServiceImpl.java
@@ -0,0 +1,59 @@
+package com.binar.finalproject.BEFlightTicket.service.impl;
+
+import com.binar.finalproject.BEFlightTicket.dto.*;
+import com.binar.finalproject.BEFlightTicket.model.*;
+import com.binar.finalproject.BEFlightTicket.repository.GatesRepository;
+import com.binar.finalproject.BEFlightTicket.service.GatesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class GatesServiceImpl implements GatesService {
+
+    @Autowired
+    private GatesRepository gatesRepository;
+
+    @Override
+    public GatesResponse addGates(GatesRequest gatesRequest) {
+        try {
+            Optional<Gates> terminals = gatesRepository.findById(gatesRequest.getTerminalId());
+            if(terminals.isPresent())
+            {
+                Gates gates = Gates.builder()
+                        .gateName(gatesRequest.getGateName())
+                        .terminalsGates(terminals.get().getTerminalsGates())
+                        .build();
+                gatesRepository.saveAndFlush(gates);
+                return GatesResponse.build(gates);
+            }
+            else
+                return null;
+        }
+        catch (Exception ignore){
+            return null;
+        }
+    }
+
+    @Override
+    public List<GatesResponse> searchAllGates() {
+        return null;
+    }
+
+    @Override
+    public GatesResponse updateGates(GatesRequest gatesRequest, String gateName) {
+        return null;
+    }
+
+    @Override
+    public Boolean deleteGates(String gateName) {
+        return null;
+    }
+
+    @Override
+    public GatesResponse searchGatesByName(String gateName) {
+        return null;
+    }
+}
