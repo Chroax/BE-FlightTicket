84aef0cb100175fd323ceaed7d926cd15b61febd
VianaTameira <190030690@stikom-bali.ac.id>
2022-11-27 00:51:17 +0700

    add route service
    - create route service interface
    - add custom query into route repository
    - create route service implementation class

--- src/main/java/com/binar/finalproject/BEFlightTicket/repository/RouteRepository.java
+++ src/main/java/com/binar/finalproject/BEFlightTicket/repository/RouteRepository.java
@@ -2,8 +2,14 @@ package com.binar.finalproject.BEFlightTicket.repository;
 
 import com.binar.finalproject.BEFlightTicket.model.Routes;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface RouteRepository extends JpaRepository<Routes, UUID> {
+    @Query("SELECT r FROM Routes r WHERE LOWER(r.departureCity) LIKE LOWER(:departureCity) AND LOWER(r.arrivalCity) LIKE LOWER(:arrivalCity) ")
+    List<Routes> findRouteByDepartureCityAndArrivalCity(@Param("departureCity") String departureCity, @Param("arrivalCity") String arrivalCity);
+
 }
---
+++ src/main/java/com/binar/finalproject/BEFlightTicket/service/impl/RouteServiceImpl.java
@@ -0,0 +1,95 @@
+package com.binar.finalproject.BEFlightTicket.service.impl;
+
+import com.binar.finalproject.BEFlightTicket.dto.*;
+import com.binar.finalproject.BEFlightTicket.model.Routes;
+import com.binar.finalproject.BEFlightTicket.repository.RouteRepository;
+import com.binar.finalproject.BEFlightTicket.service.RouteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class RouteServiceImpl implements RouteService {
+
+    @Autowired
+    RouteRepository routeRepository;
+
+    @Override
+    public RouteResponse addRoute(RouteRequest routeRequest) {
+        Routes routes = routeRequest.toRoutes();
+
+        try {
+            routeRepository.save(routes);
+            return RouteResponse.build(routes);
+        }
+        catch(Exception exception)
+        {
+            return null;
+        }
+    }
+
+    @Override
+    public List<RouteResponse> findByDepartureCityAndArrivalCity(String departureCity, String arrivalCity) {
+        List<Routes> allRoute = routeRepository.findRouteByDepartureCityAndArrivalCity(departureCity, arrivalCity);
+        List<RouteResponse> allRouteResponse = new ArrayList<>();
+        for (Routes routes : allRoute)
+        {
+            RouteResponse routeResponse = RouteResponse.build(routes);
+            allRouteResponse.add(routeResponse);
+        }
+        return allRouteResponse;
+    }
+
+
+    @Override
+    public List<RouteResponse> getAllRoute() {
+        List<Routes> allRoute = routeRepository.findAll();
+        List<RouteResponse> allRouteResponse = new ArrayList<>();
+        for (Routes routes : allRoute)
+        {
+            RouteResponse routeResponse = RouteResponse.build(routes);
+            allRouteResponse.add(routeResponse);
+        }
+        return allRouteResponse;
+    }
+
+    @Override
+    public RouteResponse updateRoute(RouteRequest routeRequest, UUID routeId) {
+        Optional<Routes> isRoutes = routeRepository.findById(routeId);
+        if (isRoutes.isPresent())
+        {
+            Routes routes = isRoutes.get();
+            routes.setDepartureCity(routeRequest.getDepartureCity());
+            routes.setArrivalCity(routeRequest.getArrivalCity());
+            routes.setDepartureAirport(routeRequest.getDepartureAirport());
+            routes.setArrivalAirport(routeRequest.getArrivalAirport());
+            routes.setDepartureTerminal(routeRequest.getDepartureTerminal());
+            routes.setArrivalTerminal(routeRequest.getArrivalTerminal());
+            try {
+                routeRepository.save(routes);
+                return RouteResponse.build(routes);
+            }
+            catch(Exception exception)
+            {
+                return null;
+            }
+        }
+        else
+            throw new RuntimeException("Route with id: " + routeId + " not found");
+    }
+
+    @Override
+    public Boolean deleteRoute(UUID routeId) {
+        Optional<Routes> isRoute = routeRepository.findById(routeId);
+        if (isRoute.isPresent()){
+            Routes routes = isRoute.get();
+            routeRepository.deleteById(routes.getRouteId());
+            return true;
+        }
+        return null;
+    }
+}
