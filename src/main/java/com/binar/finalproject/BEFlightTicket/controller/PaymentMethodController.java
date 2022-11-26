a10aa7ea5e7d960479bb7d28f56dc40316871cbb
VianaTameira <190030690@stikom-bali.ac.id>
2022-11-26 16:32:44 +0700

    add PaymentMethodController class

---
+++ src/main/java/com/binar/finalproject/BEFlightTicket/controller/PaymentMethodController.java
@@ -0,0 +1,9 @@
+package com.binar.finalproject.BEFlightTicket.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("payment")
+public class PaymentMethodController {
+}
