7745514a215651b208b6e5a96702b2e636f395f3
VianaTameira <190030690@stikom-bali.ac.id>
2022-11-27 22:45:41 +0700

    add ticket service
    - create TicketService interface
    - add custom query into TicketRepository
    - create TicketServiceImpl

--- src/main/java/com/binar/finalproject/BEFlightTicket/repository/TicketRepository.java
+++ src/main/java/com/binar/finalproject/BEFlightTicket/repository/TicketRepository.java
@@ -2,8 +2,13 @@ package com.binar.finalproject.BEFlightTicket.repository;
 
 import com.binar.finalproject.BEFlightTicket.model.Tickets;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface TicketRepository extends JpaRepository<Tickets, UUID> {
+    @Query(nativeQuery = true, value = "SELECT * FROM tickets t where t.order_id = :orderId")
+    List<Tickets> getAllOrderTicket(@Param("orderId") UUID orderId);
 }
