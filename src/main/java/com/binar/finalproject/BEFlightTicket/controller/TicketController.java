c57824cef6a2247604ec061761c20217cc07715f
VianaTameira <190030690@stikom-bali.ac.id>
2022-11-27 23:33:43 +0700

    create endpoint /ticket/add
    - create feature add ticket

--- src/main/java/com/binar/finalproject/BEFlightTicket/controller/TicketController.java
+++ src/main/java/com/binar/finalproject/BEFlightTicket/controller/TicketController.java
@@ -1,7 +1,10 @@
 package com.binar.finalproject.BEFlightTicket.controller;
 
+import com.binar.finalproject.BEFlightTicket.dto.*;
 import com.binar.finalproject.BEFlightTicket.service.TicketService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -10,5 +13,25 @@ import org.springframework.web.bind.annotation.*;
 public class TicketController {
     @Autowired
     private TicketService ticketService;
-    
+
+    @PostMapping("/add")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<MessageModel> addTicket (@RequestBody TicketRequest ticketRequest)
+    {
+        MessageModel messageModel = new MessageModel();
+        TicketResponse ticketResponse = ticketService.addTicket(ticketRequest);
+        if(ticketResponse == null)
+        {
+            messageModel.setStatus(HttpStatus.BAD_REQUEST.value());
+            messageModel.setMessage("Failed to add ticket");
+            return ResponseEntity.badRequest().body(messageModel);
+        }
+        else
+        {
+            messageModel.setStatus(HttpStatus.OK.value());
+            messageModel.setMessage("Add new ticket");
+            messageModel.setData(ticketResponse);
+            return ResponseEntity.ok().body(messageModel);
+        }
+    }
 }
