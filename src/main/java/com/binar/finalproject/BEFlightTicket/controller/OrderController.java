8e3e14d59303accfaa693fca1d33bc8aed590e17
VianaTameira <190030690@stikom-bali.ac.id>
2022-11-27 13:58:52 +0700

    create OrderController Class

---
+++ src/main/java/com/binar/finalproject/BEFlightTicket/controller/OrderController.java
@@ -0,0 +1,18 @@
+package com.binar.finalproject.BEFlightTicket.controller;
+
+import com.binar.finalproject.BEFlightTicket.dto.*;
+import com.binar.finalproject.BEFlightTicket.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+    @Autowired
+    private OrderService orderService;
+
+}
