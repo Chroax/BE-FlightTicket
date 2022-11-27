d5862ca1cbcfa489c3edf9697242109a9d9b9bc6
VianaTameira <190030690@stikom-bali.ac.id>
2022-11-27 17:02:34 +0700

    create endpoint /schedule/add
    - create feature add schedule

--- src/main/java/com/binar/finalproject/BEFlightTicket/controller/ScheduleController.java
+++ src/main/java/com/binar/finalproject/BEFlightTicket/controller/ScheduleController.java
@@ -1,12 +1,36 @@
 package com.binar.finalproject.BEFlightTicket.controller;
 
+import com.binar.finalproject.BEFlightTicket.dto.*;
 import com.binar.finalproject.BEFlightTicket.service.ScheduleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/schedule")
 public class ScheduleController {
     @Autowired
     private ScheduleService scheduleService;
+    @PostMapping("/add")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<MessageModel> addSchedule (@RequestBody ScheduleRequest scheduleRequest)
+    {
+        MessageModel messageModel = new MessageModel();
+        ScheduleResponse scheduleResponse = scheduleService.addSchedule(scheduleRequest);
+        if(scheduleResponse == null)
+        {
+            messageModel.setStatus(HttpStatus.BAD_REQUEST.value());
+            messageModel.setMessage("Failed to add schedule");
+            return ResponseEntity.badRequest().body(messageModel);
+        }
+        else
+        {
+            messageModel.setStatus(HttpStatus.OK.value());
+            messageModel.setMessage("Add new schedule");
+            messageModel.setData(scheduleResponse);
+            return ResponseEntity.ok().body(messageModel);
+        }
+    }
 }
