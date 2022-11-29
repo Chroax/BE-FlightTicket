621e231b2d085a6b5185fdedaf64f310bd202bc8
Cahyadi Surya Nugraha <cahyadisn6@gmail.com>
2022-11-29 11:39:36 +0700

    Delete method searchAirplaneTicketSchedule in SearchScheduleResponse
    - Delete method searchAirplaneTicketSchedule

--- src/main/java/com/binar/finalproject/BEFlightTicket/dto/SearchScheduleResponse.java
+++ src/main/java/com/binar/finalproject/BEFlightTicket/dto/SearchScheduleResponse.java
@@ -1,9 +1,5 @@
 package com.binar.finalproject.BEFlightTicket.dto;
 
-import com.binar.finalproject.BEFlightTicket.model.Airplanes;
-import com.binar.finalproject.BEFlightTicket.model.Routes;
-import com.binar.finalproject.BEFlightTicket.model.Schedules;
-import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -11,7 +7,6 @@ import java.time.LocalTime;
 import java.util.UUID;
 
 @Data
-@Builder
 public class SearchScheduleResponse {
     private UUID scheduleId;
     private LocalDate departureDate;
@@ -31,25 +26,4 @@ public class SearchScheduleResponse {
     private String arrivalAirport;
     private String departureTerminal;
     private String arrivalTerminal;
-
-    public static SearchScheduleResponse searchAirplaneTicketSchedule(Schedules schedules, Routes routes, Airplanes airplanes){
-        return SearchScheduleResponse.builder()
-                .scheduleId(schedules.getScheduleId())
-                .departureDate(schedules.getDepartureDate())
-                .arrivalDate(schedules.getArrivalDate())
-                .departureTime(schedules.getDepartureTime())
-                .arrivalTime(schedules.getArrivalTime())
-                .price(schedules.getPrice())
-                .status(schedules.getStatus())
-                .airplaneName(airplanes.getAirplaneName())
-                .airplaneType(airplanes.getAirplaneType())
-                .routeId(routes.getRouteId())
-                .departureCity(routes.getDepartureCity())
-                .arrivalCity(routes.getArrivalCity())
-                .departureAirport(routes.getDepartureAirport())
-                .arrivalAirport(routes.getArrivalAirport())
-                .departureTerminal(routes.getDepartureTerminal())
-                .arrivalTerminal(routes.getArrivalTerminal())
-                .build();
-    }
 }
