b5820d39a7acba05e269b60981d96c3f3b11898c
Cahyadi Surya Nugraha <cahyadisn6@gmail.com>
2022-11-30 01:06:19 +0700

    add Login dto
    - add LoginRequest class
    - add LoginResponse class

--- src/main/java/com/binar/finalproject/BEFlightTicket/dto/LoginRequest.java
+++ src/main/java/com/binar/finalproject/BEFlightTicket/dto/LoginRequest.java
@@ -1,33 +1,17 @@
 package com.binar.finalproject.BEFlightTicket.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 import javax.validation.constraints.NotBlank;
 
+@Data
+@AllArgsConstructor
 public class LoginRequest {
 
     @NotBlank
-    private String fullName;
+    private String email;
 
     @NotBlank
     private String password;
-
-    public LoginRequest(String fullName, String password) {
-        this.fullName = fullName;
-        this.password = password;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
