e97544f0963a348539bbb3a1c1f009e7b528dd78
Cahyadi Surya Nugraha <cahyadisn6@gmail.com>
2022-11-29 11:16:31 +0700

    Revert "update ScheduleRepository"
    This reverts commit b7b75f4300097ab56ded05ce0df21c62b7d66518.

--- src/main/java/com/binar/finalproject/BEFlightTicket/repository/ScheduleRepository.java
+++ src/main/java/com/binar/finalproject/BEFlightTicket/repository/ScheduleRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
@@ -14,9 +13,4 @@ public interface ScheduleRepository extends JpaRepository<Schedules, UUID> {
     List<Schedules> getAllAirplaneSchedule(@Param("airplaneName") String airplaneName);
     @Query(nativeQuery = true, value = "SELECT * FROM schedules s where s.route_id = :routeId")
     List<Schedules> getAllRouteSchedule(@Param("routeId") UUID routeId);
-    @Query("SELECT s FROM Schedules s WHERE LOWER(s.departureCitySchedule) LIKE LOWER(:departureCitySchedule) " +
-            "AND LOWER(s.arrivalCitySchedule) LIKE LOWER(:arrivalCitySchedule) AND LOWER(s.departureDate) LIKE LOWER(:departureDate) ")
-    List<Schedules> findAirplaneTicketSchedule(@Param("departureCity") String departureCity,
-                                               @Param("arrivalCity") String arrivalCity,
-                                               @Param("departureDate") LocalDate departureDate);
 }
