0860c56ecd4bd286804aee848f994a9a88c9553d
VianaTameira <190030690@stikom-bali.ac.id>
2022-11-30 01:06:18 +0700

    Update schedule, route, and service repository
    - add custom query

--- src/main/java/com/binar/finalproject/BEFlightTicket/repository/ScheduleRepository.java
+++ src/main/java/com/binar/finalproject/BEFlightTicket/repository/ScheduleRepository.java
@@ -1,6 +1,5 @@
 package com.binar.finalproject.BEFlightTicket.repository;
 
-import com.binar.finalproject.BEFlightTicket.dto.SearchScheduleResponse;
 import com.binar.finalproject.BEFlightTicket.model.Schedules;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -16,6 +15,8 @@ public interface ScheduleRepository extends JpaRepository<Schedules, UUID> {
     @Query(nativeQuery = true, value = "SELECT * FROM schedules s where s.route_id = :routeId")
     List<Schedules> getAllRouteSchedule(@Param("routeId") UUID routeId);
 
-    @Query(nativeQuery = true, value = "SELECT * FROM schedules s inner join routes r on s.route_id = r.route_id inner join airplanes a on a.airplane_name = s.airplane_name where r.departure_airport = :departureAirport and r.arrival_airport = :arrivalAirport and s.departure_date = :departureDate")
-    List<SearchScheduleResponse> searchScheduleTicket(@Param("departureAirport") String departureAirport, @Param("arrivalAirport") String arrivalAirport, @Param("departureDate") LocalDate departureDate);
+
+    @Query(nativeQuery = true, value = "SELECT*FROM schedules s INNER JOIN routes r ON s.route_id = r.route_id INNER JOIN airplanes a ON s.airplane_name = a.airplane_name where r.departure_airport = :departureAirport and r.arrival_airport = :arrivalAirport and s.departure_date = :departureDate")
+    List<Schedules> searchScheduleTicket(@Param("departureAirport") String departureAirport, @Param("arrivalAirport") String arrivalAirport, @Param("departureDate") LocalDate departureDate);
+
 }
