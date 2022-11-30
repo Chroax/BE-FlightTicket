0a18ed828f71e683ddf1919e88d98b402829ef2e
Cahyadi Surya Nugraha <cahyadisn6@gmail.com>
2022-11-30 20:22:40 +0700

    fixed syntax custom query

--- src/main/java/com/binar/finalproject/BEFlightTicket/repository/OrderRepository.java
+++ src/main/java/com/binar/finalproject/BEFlightTicket/repository/OrderRepository.java
@@ -9,8 +9,8 @@ import java.util.List;
 import java.util.UUID;
 
 public interface OrderRepository extends JpaRepository<Orders, UUID> {
-    @Query(nativeQuery = true, value = "SELECT * FROM orders o where o.userId = :userId")
+    @Query(nativeQuery = true, value = "SELECT * FROM orders o where o.user_id = :userId")
     List<Orders> findAllOrderByUserId(@Param("userId") UUID userId);
-    @Query(nativeQuery = true, value = "SELECT * FROM orders o where o.paymentId = :paymentId")
+    @Query(nativeQuery = true, value = "SELECT * FROM orders o where o.payment_id = :paymentId")
     List<Orders> findAllOrderByPaymentId(@Param("paymentId") Integer paymentId);
 }
