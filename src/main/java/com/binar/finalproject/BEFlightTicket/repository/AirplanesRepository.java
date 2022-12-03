71ec2ff9cde98ef80b83ae648fd190e7609e2fc6
VianaTameira <190030690@stikom-bali.ac.id>
2022-12-03 16:06:25 +0700

    add custom query implement pageable

--- src/main/java/com/binar/finalproject/BEFlightTicket/repository/AirplanesRepository.java
+++ src/main/java/com/binar/finalproject/BEFlightTicket/repository/AirplanesRepository.java
@@ -3,6 +3,8 @@ package com.binar.finalproject.BEFlightTicket.repository;
 import com.binar.finalproject.BEFlightTicket.model.Airplanes;
 import com.binar.finalproject.BEFlightTicket.model.Routes;
 import com.binar.finalproject.BEFlightTicket.model.Schedules;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -26,4 +28,6 @@ public interface AirplanesRepository extends JpaRepository <Airplanes, String> {
     List<Airplanes> searchScheduleTicketByEarliestArrivalTime(@Param("departureAirport") String departureAirport, @Param("arrivalAirport") String arrivalAirport, @Param("departureDate") LocalDate departureDate);
     @Query(nativeQuery = true, value = "SELECT*FROM schedules s INNER JOIN routes r ON s.route_id = r.route_id INNER JOIN airplanes a ON s.airplane_name = a.airplane_name where r.departure_airport = :departureAirport and r.arrival_airport = :arrivalAirport and s.departure_date = :departureDate ORDER BY s.arrival_time DESC ;")
     List<Airplanes> searchScheduleTicketByLatestArrivalTime(@Param("departureAirport") String departureAirport, @Param("arrivalAirport") String arrivalAirport, @Param("departureDate") LocalDate departureDate);
+    @Query("SELECT a FROM Airplanes a INNER JOIN Schedules s ON s.airplanesSchedules = a.airplaneName INNER JOIN Routes r ON s.routesSchedules = r.routeId where r.departureAirport = :departureAirport and r.arrivalAirport = :arrivalAirport and s.departureDate = :departureDate")
+    Page<Airplanes> searchTicket (@Param("departureAirport") String departureAirport, @Param("arrivalAirport") String arrivalAirport, @Param("departureDate") LocalDate departureDate, Pageable pageable);
 }
--- src/main/java/com/binar/finalproject/BEFlightTicket/repository/ScheduleRepository.java
+++ src/main/java/com/binar/finalproject/BEFlightTicket/repository/ScheduleRepository.java
@@ -2,6 +2,8 @@ package com.binar.finalproject.BEFlightTicket.repository;
 
 import com.binar.finalproject.BEFlightTicket.dto.SearchScheduleResponse;
 import com.binar.finalproject.BEFlightTicket.model.Schedules;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -33,4 +35,6 @@ public interface ScheduleRepository extends JpaRepository<Schedules, UUID> {
     List<Schedules> searchScheduleTicketByEarliestArrivalTime(@Param("departureAirport") String departureAirport, @Param("arrivalAirport") String arrivalAirport, @Param("departureDate") LocalDate departureDate);
     @Query(nativeQuery = true, value = "SELECT*FROM schedules s INNER JOIN routes r ON s.route_id = r.route_id INNER JOIN airplanes a ON s.airplane_name = a.airplane_name where r.departure_airport = :departureAirport and r.arrival_airport = :arrivalAirport and s.departure_date = :departureDate ORDER BY s.arrival_time DESC ;")
     List<Schedules> searchScheduleTicketByLatestArrivalTime(@Param("departureAirport") String departureAirport, @Param("arrivalAirport") String arrivalAirport, @Param("departureDate") LocalDate departureDate);
+    @Query("SELECT s FROM Schedules s INNER JOIN Routes r ON s.routesSchedules = r.routeId INNER JOIN Airplanes a ON s.airplanesSchedules = a.airplaneName where r.departureAirport = :departureAirport and r.arrivalAirport = :arrivalAirport and s.departureDate = :departureDate")
+    Page<Schedules> searchTicket (@Param("departureAirport") String departureAirport, @Param("arrivalAirport") String arrivalAirport, @Param("departureDate") LocalDate departureDate, Pageable pageable);
 }
