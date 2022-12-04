a299f5d5a03b71d0ec7281eb77124b81d550356d
Cahyadi Surya Nugraha <cahyadisn6@gmail.com>
2022-12-05 05:42:39 +0700

    fixed invoice for controller

--- src/main/java/com/binar/finalproject/BEFlightTicket/controller/InvoiceController.java
+++ src/main/java/com/binar/finalproject/BEFlightTicket/controller/InvoiceController.java
@@ -2,9 +2,9 @@ package com.binar.finalproject.BEFlightTicket.controller;
 
 import com.binar.finalproject.BEFlightTicket.dto.MessageModel;
 import com.binar.finalproject.BEFlightTicket.service.InvoiceService;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperPrint;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.InputStreamResource;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.ByteArrayInputStream;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @RestController
@@ -23,25 +23,22 @@ public class InvoiceController {
     @Autowired
     InvoiceService invoiceService;
 
+    @Autowired
+    HttpServletResponse response;
+
     @GetMapping("/generate/{orderId}")
-    public ResponseEntity generatePdf(@PathVariable UUID orderId)
+    public void ResponseEntity(@PathVariable UUID orderId) throws Exception
     {
         MessageModel messageModel = new MessageModel();
-        ByteArrayInputStream inputStream = invoiceService.generateInvoice(orderId);
-
-        if (inputStream.available() > 0) {
-            HttpHeaders headers = new HttpHeaders();
-            headers.add("Content-Disposition", "attachment; filename=Invoice-MTicket-" + orderId + ".pdf");
-
-            return ResponseEntity
-                    .ok()
-                    .headers(headers)
-                    .contentType(MediaType.APPLICATION_OCTET_STREAM)
-                    .body(new InputStreamResource(inputStream));
-        } else {
-            messageModel.setStatus(HttpStatus.NO_CONTENT.value());
-            messageModel.setMessage("Booking data not found");
-            return ResponseEntity.ok().body(messageModel);
+        JasperPrint jasperPrint = invoiceService.generateInvoice(orderId);
+        if(jasperPrint != null)
+        {
+            response.setContentType(MediaType.APPLICATION_PDF_VALUE);
+            response.setHeader("Content-Disposition", "attachment; filename=Invoice-MTicket-" + orderId + ".pdf");
+            messageModel.setStatus(HttpStatus.OK.value());
+            messageModel.setMessage("Generated invoice");
+            JasperExportManager.exportReportToPdfStream(jasperPrint, response.getOutputStream());
+            response.flushBuffer();
         }
     }
 }
