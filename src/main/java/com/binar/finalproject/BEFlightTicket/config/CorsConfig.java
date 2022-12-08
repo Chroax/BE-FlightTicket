065d0eceda91e5e2ee4ec93e02a32c83c41db50b
Cahyadi Surya Nugraha <cahyadisn6@gmail.com>
2022-12-08 18:03:59 +0700

    update cors to allowed all

--- src/main/java/com/binar/finalproject/BEFlightTicket/config/CorsConfig.java
+++ src/main/java/com/binar/finalproject/BEFlightTicket/config/CorsConfig.java
@@ -20,7 +20,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins(allowedOrigin)
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedMethods("*")
                         .allowedHeaders("*");
             }
         };
