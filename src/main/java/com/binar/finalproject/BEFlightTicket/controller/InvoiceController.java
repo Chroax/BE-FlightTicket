cc963d6a615b9418b224eb89ba17a455671ef3a6
VianaTameira <190030690@stikom-bali.ac.id>
2022-12-23 17:55:04 +0700

    Update InvoiceController.java

--- src/main/java/com/binar/finalproject/BEFlightTicket/controller/InvoiceController.java
+++ src/main/java/com/binar/finalproject/BEFlightTicket/controller/InvoiceController.java
@@ -8,10 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
@@ -26,8 +23,8 @@ public class InvoiceController {
     @Autowired
     HttpServletResponse response;
 
-    @GetMapping("/generate/{orderId}")
-    public void ResponseEntity(@PathVariable UUID orderId) throws Exception
+    @GetMapping("/generate")
+    public void ResponseEntity(@RequestParam UUID orderId) throws Exception
     {
         MessageModel messageModel = new MessageModel();
         JasperPrint jasperPrint = invoiceService.generateInvoice(orderId);
