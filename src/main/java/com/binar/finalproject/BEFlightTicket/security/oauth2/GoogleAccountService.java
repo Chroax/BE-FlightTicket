913645a56efeee894f94ed36d37c9af79482190b
VianaTameira <190030690@stikom-bali.ac.id>
2023-01-02 09:01:51 +0700

    Update GoogleAccountService.java

--- src/main/java/com/binar/finalproject/BEFlightTicket/security/oauth2/GoogleAccountService.java
+++ src/main/java/com/binar/finalproject/BEFlightTicket/security/oauth2/GoogleAccountService.java
@@ -1,21 +1,42 @@
 package com.binar.finalproject.BEFlightTicket.security.oauth2;
 
-import lombok.NonNull;
-import lombok.extern.slf4j.Slf4j;
+import com.binar.finalproject.BEFlightTicket.model.AuthenticationProvider;
+import com.binar.finalproject.BEFlightTicket.model.Roles;
+import com.binar.finalproject.BEFlightTicket.model.Users;
+import com.binar.finalproject.BEFlightTicket.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Map;
-import java.util.UUID;
 
-@Slf4j
 @Service
 public class GoogleAccountService {
-    public UUID findOrRegisterAccount(
-            @NonNull String socialUserId,
-            @NonNull String socialUserProvider,
-            @NonNull Map<String, Object> socialUserInfo
-    ) {
-        log.info("Looking up or registering social user; id={}; provider={}; info={}", socialUserId, socialUserProvider, socialUserInfo);
-        return UUID.randomUUID();
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public Users oAuthLoginSuccess(String email, String fullName, String googleId) {
+        Users userGoogle = userRepository.findByGoogleId(googleId);
+        if (userGoogle == null) {
+            Users userWithGmail = userRepository.findByGmail(email);
+            if (userWithGmail == null) {
+                Users users = new Users();
+                Roles roles = new Roles();
+                users.setEmail(email);
+                users.setFullName(fullName);
+                users.setStatusActive(true);
+                users.setAuthProvider(AuthenticationProvider.GOOGLE);
+                users.setGoogleId(googleId);
+                roles.setRoleName("ROLE_BUYER");
+                oAuth2Password(users);
+                return userRepository.save(users);
+            }
+        }
+        return userGoogle;
+    }
+    public void oAuth2Password(Users users){
+        String googleId = users.getGoogleId();
+        String clientId = "953090499155-f5pgpt16s6lhge53hhi4s5cm5dg18in3.apps.googleusercontent.com";
+        String googlePassword = googleId + clientId;
+        users.setPassword(String.valueOf(googlePassword.hashCode()));
     }
 }
